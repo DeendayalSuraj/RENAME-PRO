4434b1930e6780b5156a9c4e0378e13951c9dc24
DeendayalSuraj <surajkumar62067542@gmail.com>
2025-01-31 21:40:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8026778102:AAEsTeZXg7kAstTC2jtmqSchmLRcZD-Vyds")
+API_ID = int(os.environ.get("API_ID", "26992030"))
+API_HASH = os.environ.get("API_HASH", "4da7d71c6bc4512a886e41aca83a5ee3")
+OWNER = int(os.environ.get("OWNER", "6476706511"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "@Deendayal_Rename4g_bot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "Deendayal_dhakadd")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002174167223"))
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://sunil664023:eieqy2jRVIbzD29r@cluster0.ezot5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "BQGb3Z4AvMTlyWgkLEzuKuobNMeGmmeMmyg2XQn0W35kxB3YMD0AebF1ihEtTarmYOSNNPB_Q7yW2kn4esXIbn7pAzGzm5f7_r7Ej3RRvEDFMoYxHGQI36HHYbLzR8MAN3LBMRHgCryPu9yqSNuHqHmnEHN6s0n6nTEBZKAUbnq-Qulod9P-S4e88i_5nLIPNoBZ9nvnQHWhY_-Kbw1T6zDOKkOfkNV4rk4NH2NHJYBAOJpyOHYwPqx23B4XCwJ4mPefq53HTEM6AhXJFpWzSQFi_0pWX-tYeKDKwbMgol8ocA7xQod9EixOOrs06V6hWwKD6LE7FwYUH2izHKH9BoeN6xuISAAAAAGCCrLPAA")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
 
 
@@ -22,6 +22,6 @@ BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f62
 
 # if you need to add verify system then dm me on telegram
 
-SHORTNER_URL = os.environ.get("SHORTNER_URL", "")
-SHORTNER_API = os.environ.get("SHORTNER_API", "")
-TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "")
+SHORTNER_URL = os.environ.get("SHORTNER_URL", "shortxlinks.com")
+SHORTNER_API = os.environ.get("SHORTNER_API", "e094f99e4e843d3b24f51569eeddaae14ebd85ff")
+TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "2")
